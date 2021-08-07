d8bd2d69f2c0683937ef2beb9bacaca32fb9a845
menezesfelipee <sa.felipem@gmail.com>
2021-08-07 16:05:51 -0300

    alteração tabela unica

--- alexandria/controllers/register.py
+++ alexandria/controllers/register.py
@@ -2,33 +2,20 @@ from flask import render_template, redirect, request
 
 from alexandria.run import app, db
 
-from alexandria.models.schemas import Author, Book, Publisher
+from alexandria.models.schemas import Book
 
 
 @app.route('/cadastrar', methods=['GET', 'POST'])
 def cadastrar():
     # codigo 
     if request.method == 'POST':
-        
-        author = request.form['author']
-        exists_author = bool(Author.query.filter_by(name=author).first())
-        if exists_author == False:
-            author = Author(name=author)
-            db.session.add(author)
-            db.session.commit()
-            
-        publisher = 'na mão'
-        publisher_name = bool(Publisher.query.filter_by(name=publisher).first())
-        if publisher_name == False:
-            publisher = Publisher(name=publisher)
-            db.session.add(publisher)
-            db.session.commit() 
-        else:
-            return redirect('cadastrar')
-        
-        
-        
-
+        author = Book(
+            request.form['book'],
+            request.form['author'],
+            'na mão',
+            )
+        db.session.add(author)
+        db.session.commit()
         return redirect('/acervo')
     
     return render_template('cadastrar.html')
--- alexandria/models/schemas.py
+++ alexandria/models/schemas.py
@@ -1,42 +1,14 @@
 from datetime import datetime
 from alexandria.run import db
-from sqlalchemy import exists
-
-class Author(db.Model):
-    __tablename__ = 'authors'
-    
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(255), nullable=False, unique=True)
 
     
-    def __init__(self, name):
-        self.name = name
-       
-
-    
-    # forma bonita de mostrar o nome do registro
-    def __repr__(self):
-        return f'<Author {self.name}>'
-    
-    
-    
-class Publisher(db.Model):
-    __tablename__ = 'publishers'
-
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(100), nullable=False, unique=True)
-    
-    
-    def __init__(self, name):
-        self.name = name
-        
-        
-        
 class Book(db.Model):
     __tablename__ = 'books'
     
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(255), nullable=False)
+    author = db.Column(db.String(255), nullable=False)
+    publisher = db.Column(db.String(255), nullable=False)
     year_published = db.Column(db.String(20))
     isbn = db.Column(db.String(255))
     synopsis = db.Column(db.Text)
@@ -47,20 +19,12 @@ class Book(db.Model):
     image = db.Column(db.String)
     download = db.Column(db.String)
     buy = db.Column(db.String)
-    published_at = db.Column(db.DateTime(), default = datetime.strftime(datetime.today(), "%b %d %Y"))
-    updated_at = db.Column(db.DateTime(), default = datetime.strftime(datetime.today(), "%b %d %Y"))
-    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'), nullable=False)
-    publisher_id = db.Column(db.Integer, db.ForeignKey('publishers.id'), nullable=False)
     
     
-    author = db.relationship('Author', backref=db.backref('book',  lazy=True))
-    publisher = db.relationship('Publisher', backref=db.backref('book',  lazy=True))
-    
-    
-    def __init__(self, name, author_id, publisher_id):
+    def __init__(self, name, author, publisher):
         self.name = name
-        self.author_id = author_id
-        self.publisher_id = publisher_id
+        self.author = author
+        self.publisher = publisher
     
         
     
