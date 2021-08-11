9694dfb256b3ab734dd7ff361214b68c0e420922
thaynar96 <thaynarbo@gmail.com>
2021-08-11 12:29:20 -0300

    modal e condicao if

--- alexandria/controllers/edit.py
+++ alexandria/controllers/edit.py
@@ -7,11 +7,6 @@ from alexandria.models.schemas import Book
 @app.route('/editar-livro/<id>', methods=['POST', 'GET'])
 def edit(id):
     book = Book.query.get(id)
-    url_img = ""
-    if book.image != None:
-        url_img = book.image
-    else:
-        url_img = '../frontend/static/img/placeholder_cadastro'
     if request.method == 'POST':
         book.name = request.form['name-book']
         book.author = request.form['name-author']
@@ -20,4 +15,4 @@ def edit(id):
         db.session.commit()
         return redirect(f'/detalhelivro/{book.id}')
     
-    return render_template('edit.html', book=book,url_img=url_img)
+    return render_template('edit.html', book=book)
--- alexandria/frontend/static/js/app.js
+++ alexandria/frontend/static/js/app.js
@@ -1,6 +1,6 @@
 const getElement = (selector) => {
-const el = document.querySelector(selector)
-  if (el) return el 
+  const el = document.querySelector(selector)
+  if (el) return el
   throw new Error(`Please check your classes : ${selector} does not exist`)
 }
 
@@ -8,10 +8,22 @@ const sidebarToggle = getElement('.sidebar-toggle')
 const sidebar = getElement('.sidebar')
 const closeBtn = getElement('.close-btn')
 
+const modalBtn = getElement('.modal-btn')
+const modal = getElement('.modal-overlay')
+const closeBtn2 = getElement('.close-btn2')
+
 sidebarToggle.addEventListener('click', function () {
   sidebar.classList.toggle('show-sidebar')
 })
 
-closeBtn.addEventListener('click', () => {
+closeBtn2.addEventListener('click', () => {
   sidebar.classList.remove('show-sidebar')
+})
+
+
+modalBtn.addEventListener('click', function () {
+  modal.classList.add('open-modal')
+})
+closeBtn2.addEventListener('click', function () {
+  modal.classList.remove('open-modal')
 })
