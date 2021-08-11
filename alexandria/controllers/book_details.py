dff19c79871f2f625bcff822986183dc36f4731d
thaynar96 <thaynarbo@gmail.com>
2021-08-11 02:04:35 -0300

    imagens corretas

--- alexandria/controllers/book_details.py
+++ alexandria/controllers/book_details.py
@@ -8,4 +8,8 @@ from alexandria.models.schemas import Book
 @app.route('/detalhelivro/<id>')
 def book_details(id):
     book = Book.query.get(id)
-    return render_template('book_details.html', book=book)
+    if book.image != None:
+            url_img = book.image
+    else:
+        url_img = '../frontend/static/img/placeholder_cadastro'
+    return render_template('book_details.html', book=book,url_img=url_img)
--- alexandria/controllers/edit.py
+++ alexandria/controllers/edit.py
@@ -7,12 +7,18 @@ from alexandria.models.schemas import Book
 @app.route('/editar-livro/<id>', methods=['POST', 'GET'])
 def edit(id):
     book = Book.query.get(id)
+    url_img = ""
+    if book.image != None:
+        url_img = book.image
+    else:
+        url_img = '../frontend/static/img/placeholder_cadastro'
     if request.method == 'POST':
         book.name = request.form['name-book']
         book.author = request.form['name-author']
         book.publisher = request.form['publisher']
         db.session.add(book)
         db.session.commit()
+
         return redirect(f'/detalhelivro/{book.id}')
     
-    return render_template('edit.html', book=book)
+    return render_template('edit.html', book=book,url_img=url_img)
