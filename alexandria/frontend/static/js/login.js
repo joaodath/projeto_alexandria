7b9de74f1d2f5100b3139dc1af1584ccf737888a
Bianca Monteiro Corrêa <83734803+Bianca-22@users.noreply.github.com>
2021-08-11 21:37:11 -0300

    Update login.js

--- alexandria/frontend/static/js/login.js
+++ alexandria/frontend/static/js/login.js
@@ -1,17 +1,3 @@
-var btnSignin = document.querySelector("#signin");
-var btnSignup = document.querySelector("#signup");
-
-var body = document.querySelector("body");
-
-
-btnSignin.addEventListener("click", function () {
-   body.className = "sign-in-js"; 
-});
-
-btnSignup.addEventListener("click", function () {
-    body.className = "sign-up-js";
-})
-
 function check_form(){
   var inputs = document.getElementsByClassName('required');
   var len = inputs.length;
@@ -20,23 +6,9 @@ function check_form(){
     if (!inputs[i].value){valid = false}
   }
   if (!valid){
-    alert('Preencha todos os campos para o cadastro.');
+    alert('Preencha todos os campos.');
     return false;
   } else{ return true }
 }
-
-function check_form_log(){
-  var inputs = document.getElementsByClassName('required_log');
-  var len = inputs.length;
-  var valid = true;
-  for (var i=0; i < len; i++){
-    if (!inputs[i].value){valid = false}
-  }
-  if (!valid){
-    alert('Preencha todos os campos para logar.');
-    return false;
-  } else { return true }
-}
-
 
   
