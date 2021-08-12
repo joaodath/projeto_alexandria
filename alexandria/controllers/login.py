fcd5be0e317f5077c1b1e0e9369a2e63c9a6f022
Bianca Monteiro Corrêa <83734803+Bianca-22@users.noreply.github.com>
2021-08-11 21:40:01 -0300

    Update login.py

--- alexandria/controllers/login.py
+++ alexandria/controllers/login.py
@@ -1,22 +1,27 @@
-from flask import render_template, redirect, request, url_for
-from alexandria.run import app, db
-from flask_login import login_user, logout_user
-
+from flask import Flask, render_template, redirect, request, url_for, session, flash
+from alexandria.run import app
+from flask_login import login_user
 from alexandria.models.schemas import User
+
+app = Flask(__name__)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        password = request.form['password']
-        
-        user = User.objects.filter(email=email).first()
-        
-        if not user or not user.verify_password(password):
-            return redirect(url_for('/'))
-        
-        login_user(user)
-        return redirect(url_for('signup'))
-    return render_template('login.html')
+        session['email'] = None
+        if request.method == 'POST':
+            email = request.form['email']
+            password = request.form['password']
+            user = User.objects.filter(email=email).first()
+            
+            if not user or not user.verify_password(password):
+                return redirect(url_for('/'))
+            
+            login_user(user)
+            flash('Login feito com sucesso!')
+            return redirect(url_for('/acervo'))
+    
+        flash('Erro no login, tente novamente!')
+        return render_template('login.html')
+
+
